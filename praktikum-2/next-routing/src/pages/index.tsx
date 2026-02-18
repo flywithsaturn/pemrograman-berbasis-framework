328f51199967851621b84a510a18792fdd52cafe
satriowisnu <satriowisnuap@email.com>
2026-02-18 10:12:58 +0700

    Langkah 3  jobsheet 3 (bagian a)

--- praktikum-2/next-routing/src/pages/index.tsx
+++ praktikum-2/next-routing/src/pages/index.tsx
@@ -14,7 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <h1>Next Routing</h1>
+      {/* <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -108,7 +109,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
     </>
   )
 }
