7d0d50c129ee27705d3bbae969e72eae5c0a41d8
satriowisnu <satriowisnuap@email.com>
2026-03-01 22:04:24 +0700

    Jobsheet 4 Langkah 8 – Simulasi Redirect (Belum Login))

--- praktikum-4/link-navigation/src/pages/produk/index.tsx
+++ praktikum-4/link-navigation/src/pages/produk/index.tsx
@@ -1,9 +1,19 @@
-const produk = () => {
-    return (
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const Produk = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    const{push} = useRouter();
+    useEffect(() => {
+        if(!isLogin){
+            push("/auth/login");
+        }
+    }, []);
+    return(
         <div>
             Produk User Page
         </div>
     );
 };
 
-export default produk;
+export default Produk;
