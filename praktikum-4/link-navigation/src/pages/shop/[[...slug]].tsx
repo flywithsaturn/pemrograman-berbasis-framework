a549556cda76d43205c3cc6337d4683ced1e4ea0
satriowisnu <satriowisnuap@email.com>
2026-03-01 21:48:50 +0700

    Jobsheet 4 (Langkah 5 – Validasi Parameter)

--- praktikum-4/link-navigation/src/pages/shop/[[...slug]].tsx
+++ praktikum-4/link-navigation/src/pages/shop/[[...slug]].tsx
@@ -10,6 +10,7 @@ const HalamanToko = () => {
             <h1>Halaman Toko</h1>
             <p>Toko: {query.slug && query.slug[0]+"-"+query.slug[1]}</p>
              {/* <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p> */}
+            <p>Kategori: {query.slug ? query.slug[0] : "Semua Kategori"}</p>
         </div>
     );
 };
