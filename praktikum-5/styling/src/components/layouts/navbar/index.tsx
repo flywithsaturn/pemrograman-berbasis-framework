fcafa38330359b73f953933860ccf019de026fdd
satriowisnu <satriowisnuap@email.com>
2026-03-03 19:09:31 +0700

    Jobsheet 5 Langkah 2 CSS Module (Local Scope) (Bagian c (Pemanggilan di Komponen))

--- praktikum-5/styling/src/components/layouts/navbar/index.tsx
+++ praktikum-5/styling/src/components/layouts/navbar/index.tsx
@@ -1,7 +1,11 @@
+import styles from '@/components/layouts/navbar/navbar.module.css'
+
 const Navbar = () => {
     return (
-        <div className="navbar">
+        <div className={styles.navbar}>
             <div>navbar component</div>
+            <br />
+            <div>Satrio Wisnu Adi Pratama</div>
         </div>
     );
 };
