0f63123e761ce0ddeaa541313224a2245e297e73
satriowisnu <satriowisnuap@email.com>
2026-03-09 23:21:42 +0700

    Jobsheet 7 Langkah 10 API Mengambil Data Firebase

--- praktikum-7/api-routes/src/pages/api/produk.ts
+++ praktikum-7/api-routes/src/pages/api/produk.ts
@@ -1,42 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { retrieveProducts } from "../utils/db/servicefirebase";
 
 type Data = {
   status: boolean;
   status_code: number;
-  data: {
-    id: number;
-    name: string;
-    price: number;
-    size: string;
-    color: string;
-  }[];
+  data: any;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const data = [
-    {
-      id: 1,
-      name: "Baju",
-      price: 100000,
-      size: "L",
-      color: "Hitam",
-    },
-    {
-      id: 2,
-      name: "Celana",
-      price: 50000,
-      size: "M",
-      color: "Hitam",
-    },
-  ];
-
-  res.status(200).json({
-    status: true,
-    status_code: 200,
-    data: data,
-  });
+  const data = await retrieveProducts("products");
+  res.status(200).json({ status: true, status_code: 200, data });
 }
