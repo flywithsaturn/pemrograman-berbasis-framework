3bb3b886f77bade85698287bb4cbfc668366f79c
satriowisnu <satriowisnuap@email.com>
2026-03-10 22:00:19 +0700

    Jobsheet 8 Langkah 5 Implementasi SWR

--- praktikum-8/csr-data-fetching/src/pages/produk/index.tsx
+++ praktikum-8/csr-data-fetching/src/pages/produk/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import TampilanProduk from "../views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
 
 // const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const kategori = () => {
@@ -14,21 +16,27 @@ const kategori = () => {
   //   }
   //   },[]);
 
-  useEffect(() => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        setProducts(responsedata.data);
-        // console.log("Data produk:", responsedata.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching produk:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/produk")
+  //     .then((response) => response.json())
+  //     .then((responsedata) => {
+  //       setProducts(responsedata.data);
+  //       // console.log("Data produk:", responsedata.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching produk:", error);
+  //     });
+  // }, []);
+
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+  //cek apakah data, error, dan isLoading sudah benar
+  // console.log("data:", data);
+  // console.log("error:", error);
+  // console.log("isLoading:", isLoading);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
   );
 };
