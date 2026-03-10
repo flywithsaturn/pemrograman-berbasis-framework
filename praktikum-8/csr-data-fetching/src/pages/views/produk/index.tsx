ccfa284387834c804801f40af2ef1e7d13b3c693
satriowisnu <satriowisnuap@email.com>
2026-03-10 22:29:48 +0700

    Jobsheet 8 Tugas 2

--- praktikum-8/csr-data-fetching/src/pages/views/produk/index.tsx
+++ praktikum-8/csr-data-fetching/src/pages/views/produk/index.tsx
@@ -9,39 +9,49 @@ type ProductType = {
 };
 
 const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+  const skeletonArray = Array.from({ length: 6 });
+
+  // Tes untuk menampilkan produk atau skeleton
+  const showProduct = true;
+
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
+
       <div className={styles.produk__content}>
-        {products.length > 0 ? (
-          <>
-            {products.map((products: ProductType) => (
-              <div key={products.id} className={styles.produk__content__item}>
+        {showProduct && products.length > 0
+          ? products.map((product: ProductType) => (
+              <div key={product.id} className={styles.produk__content__item}>
                 <div className={styles.produk__content__item__image}>
-                  <img src={products.image} alt={products.name} width={200} />
+                  <img src={product.image} alt={product.name} />
                 </div>
+
                 <h4 className={styles.produk__content__item__name}>
-                  {products.name}
+                  {product.name}
                 </h4>
+
                 <p className={styles.produk__content__item__category}>
-                  {products.category}
+                  {product.category}
                 </p>
+
                 <p className={styles.produk__content__item__price}>
-                  Rp {products.price.toLocaleString()}
+                  Rp {product.price.toLocaleString("id-ID")}
                 </p>
               </div>
+            ))
+          : skeletonArray.map((_, index) => (
+              <div key={index} className={styles.produk__content__skeleton}>
+                <div className={styles.produk__content__skeleton__image}></div>
+                <div className={styles.produk__content__skeleton__name}></div>
+                <div
+                  className={styles.produk__content__skeleton__category}
+                ></div>
+                <div className={styles.produk__content__skeleton__price}></div>
+              </div>
             ))}
-          </>
-        ) : (
-          <div className={styles.produk__content__skeleton}>
-            <div className={styles.produk__content__skeleton__image}></div>
-            <div className={styles.produk__content__skeleton__name}></div>
-            <div className={styles.produk__content__skeleton__category}></div>
-            <div className={styles.produk__content__skeleton__price}></div>
-          </div>
-        )}
       </div>
     </div>
   );
 };
+
 export default TampilanProduk;
